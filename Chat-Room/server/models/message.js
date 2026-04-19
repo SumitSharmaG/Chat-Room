56b839433c0bddf437f56edc420d073c9d399778
SumitSharmaG <sumitsharma2jul@gmail.com>
2026-04-19 07:01:26 +0530

    Delete Chat-Room directory

--- Chat-Room/server/models/message.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const MessageSchema = new mongoose.Schema({
-  username: String,
-  text: String
-});
-
-module.exports = mongoose.model("Message", MessageSchema);
