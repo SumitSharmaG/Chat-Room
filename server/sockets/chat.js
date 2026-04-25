2e4a8389bf461d85f1fb796deafe253046229c21
SumitSharmaG <sumitsharma2jul@gmail.com>
2026-04-25 06:53:47 +0530

    Update chat.js

--- server/sockets/chat.js
+++ server/sockets/chat.js
@@ -1,39 +1,13 @@
 const Message = require("../models/Message");
 
 module.exports = (io) => {
-  let onlineCount = 0;
-
   io.on("connection", (socket) => {
-    onlineCount++;
-    io.emit("updateUserCount", onlineCount);
+    console.log("User connected");
 
     socket.on("sendMessage", async (data) => {
-      const finalData = {
-        ...data,
-        id: data.id || "msg_" + Date.now() + "_" + Math.floor(Math.random() * 1000)
-      };
-      
-      // Pehle broadcast karo taaki screen par turant dikhe
-      io.emit("receiveMessage", finalData);
+      const msg = await Message.create(data);
 
-      // Save to DB (Only if not a system alert)
-      try {
-        if (Message && data.username !== "SYSTEM") {
-          await Message.create(finalData);
-        }
-      } catch (err) {
-        console.log("DB Storage skipped/error:", err.message);
-      }
-    });
-
-    socket.on("typing", (data) => socket.broadcast.emit("displayTyping", data));
-    socket.on("stopTyping", () => socket.broadcast.emit("removeTyping"));
-    socket.on("messageSeenUpdate", (data) => io.emit("userSeenTheMessage", data));
-    socket.on("clearAllChat", () => io.emit("chatCleared"));
-
-    socket.on("disconnect", () => {
-      onlineCount--;
-      io.emit("updateUserCount", onlineCount);
+      io.emit("receiveMessage", msg);
     });
   });
 };
