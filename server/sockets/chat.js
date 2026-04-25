ba5601337c8b98f95faf34ff8aacfbc129576edc
SumitSharmaG <sumitsharma2jul@gmail.com>
2026-04-25 06:11:13 +0530

    Enhance chat functionality with user count and error handling
    Added online user count tracking and improved message handling with fallback ID.

--- server/sockets/chat.js
+++ server/sockets/chat.js
@@ -1,13 +1,50 @@
 const Message = require("../models/Message");
 
 module.exports = (io) => {
+  let onlineCount = 0;
+
   io.on("connection", (socket) => {
-    console.log("User connected");
+    onlineCount++;
+    io.emit("updateUserCount", onlineCount);
 
+    // 1. Message Send & Receive
     socket.on("sendMessage", async (data) => {
-      const msg = await Message.create(data);
+      // Free version: agar backend model crash ho to bhi msg jaye isliye fallback ID
+      const finalData = {
+        ...data,
+        id: data.id || "msg_" + Date.now() + "_" + Math.floor(Math.random() * 1000)
+      };
+      
+      try {
+        const msg = await Message.create(finalData);
+        io.emit("receiveMessage", msg);
+      } catch (err) {
+        io.emit("receiveMessage", finalData);
+      }
+    });
 
-      io.emit("receiveMessage", msg);
+    // 2. Typing Indicator
+    socket.on("typing", (data) => {
+      socket.broadcast.emit("displayTyping", data);
+    });
+
+    socket.on("stopTyping", () => {
+      socket.broadcast.emit("removeTyping");
+    });
+
+    // 3. Seen Feature Signal
+    socket.on("messageSeenUpdate", (data) => {
+      io.emit("userSeenTheMessage", data);
+    });
+
+    socket.on("clearAllChat", () => {
+      io.emit("chatCleared");
+    });
+
+    socket.on("disconnect", () => {
+      onlineCount--;
+      io.emit("updateUserCount", onlineCount);
+      socket.broadcast.emit("removeTyping");
     });
   });
 };
