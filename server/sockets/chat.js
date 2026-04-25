ba9ce1dd2a649faa6e988de6e78dedc9f82390b4
SumitSharmaG <sumitsharma2jul@gmail.com>
2026-04-25 07:38:48 +0530

    Update chat.js

--- server/sockets/chat.js
+++ server/sockets/chat.js
@@ -1,13 +1,56 @@
 const Message = require("../models/Message");
 
+// 🔥 UNIQUE USERS TRACK
+const onlineUsers = new Map();
+
 module.exports = (io) => {
   io.on("connection", (socket) => {
     console.log("User connected");
 
-    socket.on("sendMessage", async (data) => {
-      const msg = await Message.create(data);
+    // 🔹 USER JOIN EVENT
+    socket.on("userJoined", (username) => {
+      socket.username = username;
 
-      io.emit("receiveMessage", msg);
+      // Agar same user multiple tabs open kare
+      // to Map me overwrite hoga (duplicate nahi banega)
+      onlineUsers.set(username, socket.id);
+
+      console.log("Online Users:", onlineUsers);
+
+      // 🔥 COUNT UPDATE
+      io.emit("updateUserCount", onlineUsers.size);
+    });
+
+    // 🔹 MESSAGE SEND (unchanged)
+    socket.on("sendMessage", async (data) => {
+      try {
+        const msg = await Message.create(data);
+        io.emit("receiveMessage", msg);
+      } catch (err) {
+        console.error("Message error:", err);
+      }
+    });
+
+    // 🔹 CLEAR CHAT (agar use kar rahe ho)
+    socket.on("clearAllChat", async () => {
+      try {
+        await Message.deleteMany({});
+        io.emit("chatCleared");
+      } catch (err) {
+        console.error("Clear error:", err);
+      }
+    });
+
+    // 🔻 DISCONNECT
+    socket.on("disconnect", () => {
+      console.log("User disconnected");
+
+      if (socket.username) {
+        onlineUsers.delete(socket.username);
+      }
+
+      // 🔥 COUNT UPDATE
+      io.emit("updateUserCount", onlineUsers.size);
     });
   });
 };
