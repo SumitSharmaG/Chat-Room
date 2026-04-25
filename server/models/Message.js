522cda0d47a381d1d7ba49eb067c99fe95f4882f
SumitSharmaG <sumitsharma2jul@gmail.com>
2026-04-25 20:45:08 +0530

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 
 const MessageSchema = new mongoose.Schema({
   username: String,
-  text: String
+  text: String,
+
+  // 🔥 ADD (already using in frontend)
+  time: String,
+
+  // 🔥 SEEN FEATURE
+  seenBy: {
+    type: [String],
+    default: []
+  }
 });
 
 module.exports = mongoose.model("Message", MessageSchema);
