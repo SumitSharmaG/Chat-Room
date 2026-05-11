7b1969d112e6c7573e20baf53d65b03c5093dad1
SumitSharmaG <sumitsharma2jul@gmail.com>
2026-05-11 07:39:02 +0530

    Update Message.js

--- server/models/Message.js
+++ server/models/Message.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 const MessageSchema = new mongoose.Schema({
   username: String,
   text: String,
-
-  // 🔥 ADD (already using in frontend)
   time: String,
-
-  // 🔥 SEEN FEATURE
+  // 🆕 Naya Field: "world" ya receiver ka username
+  receiver: {
+    type: String,
+    default: "world"
+  },
   seenBy: {
     type: [String],
     default: []
