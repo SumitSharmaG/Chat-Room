861560970523bcb6fbb315c9b3ad35da0842a63f
TheSumitSharma <sumitsharma2jul@gmail.com>
2026-06-08 17:58:20 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,8 @@
 const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    index: true,
-    lowercase: true,
-    trim: true,
-    minlength: 3,
-    maxlength: 20,
-    match: /^[a-z0-9._]+$/
-  },
-
-  password: {
-    type: String,
-    required: true
-  }
+username: String,
+password: String
 });
 
 module.exports = mongoose.model("User", UserSchema);
