3aadbde9a31ad4221d0bb1860aa5faedd641a71e
TheSumitSharma <sumitsharma2jul@gmail.com>
2026-07-13 11:40:19 +0530

    Update attachment.js

--- server/sockets/attachment.js
+++ server/sockets/attachment.js
@@ -1,1 +1,28 @@
+module.exports = function attachmentSocket(io, socket) {
 
+    socket.on("sendAttachment", (data) => {
+
+        if (!data) return;
+
+        io.emit("receiveAttachment", {
+            id: Date.now().toString() +
+                Math.random().toString(36).substring(2, 8),
+
+            username: data.username,
+
+            fileName: data.fileName,
+
+            fileType: data.fileType,
+
+            fileSize: data.fileSize,
+
+            mimeType: data.mimeType,
+
+            dataUrl: data.dataUrl,
+
+            time: data.time
+        });
+
+    });
+
+};
