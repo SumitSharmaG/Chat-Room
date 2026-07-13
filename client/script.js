840e81d80c554983e936ce0a994b0f4391c669db
TheSumitSharma <sumitsharma2jul@gmail.com>
2026-07-13 14:29:04 +0530

    Update script.js

--- client/script.js
+++ client/script.js
@@ -320,13 +320,8 @@ function displayMessage(data) {
     }
 
     messagesUl.appendChild(li);
-scrollToBottom();
+    scrollToBottom();
 
-localStorage.setItem(
-    "chat_history",
-    messagesUl.innerHTML
-);
-}
   
 
 // ================= ATTACHMENT PREVIEW =================
@@ -511,4 +506,6 @@ window.sendAttachment = function(fileData){
         time:getCurrentTime()
 
     });
-};                                                }                              
+
+};
+                                             
