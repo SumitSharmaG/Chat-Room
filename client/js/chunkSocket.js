5f1dd0ca6e02b8fff5764c2b379f5cd214008a8d
TheSumitSharma <sumitsharma2jul@gmail.com>
2026-07-14 21:15:06 +0530

    Update chunkSocket.js

--- client/js/chunkSocket.js
+++ client/js/chunkSocket.js
@@ -1,1 +1,63 @@
+// ===========================================
+// Secure Ultra Chat
+// chunkSocket.js
+// Socket.IO Chunk Relay
+// ===========================================
 
+module.exports = function(io){
+
+    io.on("connection",(socket)=>{
+
+        // ===========================
+        // Receive Chunk
+        // ===========================
+
+        socket.on("uploadChunk",(data)=>{
+
+            if(!data) return;
+
+            io.emit("receiveChunk",{
+
+                fileId:data.fileId,
+
+                fileName:data.fileName,
+
+                fileSize:data.fileSize,
+
+                mimeType:data.mimeType,
+
+                fileType:data.fileType,
+
+                chunkIndex:data.chunkIndex,
+
+                totalChunks:data.totalChunks,
+
+                chunkData:data.chunkData,
+
+                username:data.username,
+
+                time:data.time
+
+            });
+
+        });
+
+        // ===========================
+        // Upload Finished
+        // ===========================
+
+        socket.on("uploadComplete",(data)=>{
+
+            io.emit("uploadCompleted",{
+
+                fileId:data.fileId,
+
+                username:data.username
+
+            });
+
+        });
+
+    });
+
+};
