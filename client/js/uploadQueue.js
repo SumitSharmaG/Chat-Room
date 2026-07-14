8f5c5e3504200460ea80dd697dbf7649e3e8bd34
TheSumitSharma <sumitsharma2jul@gmail.com>
2026-07-14 22:29:24 +0530

    Update uploadQueue.js

--- client/js/uploadQueue.js
+++ client/js/uploadQueue.js
@@ -1,1 +1,47 @@
+// ==========================================
+// Upload Queue
+// ==========================================
 
+const UploadQueue = {
+
+    queue: [],
+
+    uploading: false,
+
+    add(file){
+
+        this.queue.push(file);
+
+        this.next();
+
+    },
+
+    async next(){
+
+        if(this.uploading) return;
+
+        if(this.queue.length===0) return;
+
+        this.uploading=true;
+
+        const file=this.queue.shift();
+
+        try{
+
+            await ChunkSender.send(file);
+
+        }catch(err){
+
+            console.error(err);
+
+        }
+
+        this.uploading=false;
+
+        this.next();
+
+    }
+
+};
+
+window.UploadQueue=UploadQueue;
