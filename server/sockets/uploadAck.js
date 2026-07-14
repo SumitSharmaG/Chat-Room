6268c4676ac2b254af076a5bbfcd5d430bf304ff
TheSumitSharma <sumitsharma2jul@gmail.com>
2026-07-14 22:31:15 +0530

    Update uploadAck.js

--- server/sockets/uploadAck.js
+++ server/sockets/uploadAck.js
@@ -1,1 +1,20 @@
+// ==========================================
+// Upload ACK
+// ==========================================
 
+module.exports = function(io, socket){
+
+    socket.on("chunk-upload", (packet)=>{
+
+        // Server confirms chunk received
+        socket.emit("chunk-ack",{
+
+            transferId:packet.transferId,
+
+            chunkIndex:packet.chunkIndex
+
+        });
+
+    });
+
+};
